97aa7396bf64ecc4d076f4e88cb2434fcf8bc7a6
Jeethendra Balaji <jeeboxical@gmail.com>
2022-12-24 15:52:20 +0530

    creates a basic template

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const operate = (operator, num1, num2) => {
+    if (operator == "+") {
+        return add(num1, num2);
+    }
+}
